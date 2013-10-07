533eee8b4e2a0cb8e81c45a9062efc4047702991
Mike C. Fletcher <mcfletch@vrplumber.com>
2013-10-07 00:26:19 -0400

    Version bump for release

--- OpenGLContext_qt/__init__.py
+++ OpenGLContext_qt/__init__.py
@@ -9,7 +9,7 @@ instead of a VRMLContext, though the VRMLContext will do more
 setup/registration when used than the QtContext.
 """
 from OpenGLContext.plugins import Context,InteractiveContext,VRMLContext
-__version__ = '1.0.0a2'
+__version__ = '1.0.0a3'
 Context( 
     'qt', 'OpenGLContext_qt.qtcontext.QtContext',
 )
