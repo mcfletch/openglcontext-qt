6f81ebcff88b96e4d4c9fffbc4b912908e10ca03
Mike C. Fletcher <mcfletch@vrplumber.com>
2026-08-01 21:46:37 -0400

    CLEANUP Docstring and general formatting fixes, python2 compatability cleanup

--- OpenGLContext_qt/__init__.py
+++ OpenGLContext_qt/__init__.py
@@ -1,21 +1,26 @@
-"""PyQt4-based OpenGLContext Context implementation
+"""PyQt4/PySide-based OpenGLContext Context implementation
 
-Note that this code is BSD licenced, but that PyQt is GPL licenced,
-you can, optionally, use the PySide library if this is a problem for you.
+Note that this code is BSD licenced, but that PyQt is GPL licenced.
+You can use the LGPL PySide library if this is a problem for you.
 
-Note: all of the Contexts defined here are defined in the same 
+Note: all of the Contexts defined here are defined in the same
 module, so there is no load-time benefit to using a QtContext
-instead of a VRMLContext, though the VRMLContext will do more 
+instead of a VRMLContext, though the VRMLContext will do more
 setup/registration when used than the QtContext.
 """
-from OpenGLContext.plugins import Context,InteractiveContext,VRMLContext
-__version__ = '1.0.0a3'
-Context( 
-    'qt', 'OpenGLContext_qt.qtcontext.QtContext',
+
+from OpenGLContext.plugins import Context, InteractiveContext, VRMLContext
+
+__version__ = "1.0.0a3"
+Context(
+    "qt",
+    "OpenGLContext_qt.qtcontext.QtContext",
 )
-InteractiveContext( 
-    'qt', 'OpenGLContext_qt.qtcontext.QtInteractiveContext',
+InteractiveContext(
+    "qt",
+    "OpenGLContext_qt.qtcontext.QtInteractiveContext",
 )
-VRMLContext( 
-    'qt', 'OpenGLContext_qt.qtcontext.VRMLContext',
+VRMLContext(
+    "qt",
+    "OpenGLContext_qt.qtcontext.VRMLContext",
 )
